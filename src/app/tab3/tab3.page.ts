2e2aa20e2082d75c6e7fa7c243c28c125bb84971
BiahMarqyes <santosmarquesbeatriz@gmail.com>
2023-03-23 08:59:01 -0300

    23-03

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -7,6 +7,11 @@ import { Component } from '@angular/core';
 })
 export class Tab3Page {
 
+  tamanho = 0;
+  dado = 'assets/dado.jpg';
+  gif = 'assets/dadogif.gif';
+  valordado = 0;
+
   constructor() {}
 
 }
